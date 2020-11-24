7453626ebd216ba9cc79974e20e213feb6dfbd8e
RobertoDIT <74931775+RobertoDIT@users.noreply.github.com>
2020-11-24 14:00:19 +0100

    Se añaden ahora las clases de Óscar

---
+++ JenkinsJSF/src/test/java/es/mpt/scsp/BasicTest.java
@@ -0,0 +1,19 @@
+package test.java.es.mpt.scsp;
+
+import main.java.es.mpt.scsp.Historico;
+
+import org.junit.Test;
+
+public class BasicTest {
+
+    @Test
+    public void testBien() {
+        Historico hist = new Historico(1,1,"prueba", 1, 1,1,"test",2);
+		assert (hist.isEstadoString()=="CORRECTO");
+    }
+    @Test
+    public void testMal() {
+        Historico hist = new Historico(1,1,"prueba", 1, 1,1,"test",2);
+		assert (hist.isEstadoString()=="asadasdasd");
+    }
+}
