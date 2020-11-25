941744fe2f64a0d9425800efae271db009b3873b
RobertoDIT <74931775+RobertoDIT@users.noreply.github.com>
2020-11-25 11:24:18 +0100

    Comentarios

--- JenkinsJSF/src/test/java/es/mpt/scsp/BasicTest.java
+++ JenkinsJSF/src/test/java/es/mpt/scsp/BasicTest.java
@@ -15,5 +15,14 @@ public class BasicTest {
     public void testMal() {
         Historico hist = new Historico(1,1,"prueba", 1, 1,1,"test",2);
 		assert (hist.isEstadoString()=="asadasdasd");
+		
+		/*
+		 * https://github.com/RobertoDIT/PracticasSGAD.git
+		 * C:\Program Files\Java\jdk1.8.0_131
+		 * C:\Archivos\Programas\apache-maven-3.6.3\bin
+		 * C:\Archivos\Programas\apache-maven-3.6.3\conf
+		 * 
+		 * 
+		 */
     }
 }
